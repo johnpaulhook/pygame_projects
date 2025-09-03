aa263d69df2994bd81fffb2c463d3bf0721f1814
John Hook <johnpaul82413@gmail.com>
2025-09-03 18:20:43 -0500

    draw line and laser

--- voidbreakers.py
+++ voidbreakers.py
@@ -10,21 +10,24 @@ WIDTH, HEIGHT = 1800, 1000            # window size
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("VoidBreakers")  # window title
 clock = pygame.time.Clock()            # controls game speed (FPS)
-
+ship_x = 250
+ship_y = 0
 stars = []
 for i in range(1001):
     star_x = random.randint(0, WIDTH)
     star_y = random.randint(0, HEIGHT)
     stars.append([star_x, star_y])
     
+    
+pygame.draw.rect(screen, (255, 255, 255) ,(ship_x, ship_y, 100, 1), thickness = 0)
 # ---------------- GAME LOOP ---------------- #
 while True:
     for event in pygame.event.get():      # check events
         if event.type == pygame.QUIT:     # click [X] button
             pygame.quit()                 # shut down pygame
             sys.exit()
-        if event.type == pygame.mouse:
-            pygame.draw.rect(screen, (255, 0, 0))
+        if event.type == pygame.K_SPACE:
+            pygame.draw.rect(screen, (255, 0, 0),ship_x, ship_y, 10, 2)
 
     # --- drawing ---
     screen.fill((0, 0, 0))             # dark background
